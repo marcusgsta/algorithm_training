a92bc1295fb31eb3bf435455e23d6e508badb894
Marcus Gustafsson <marcusgu@hotmail.com>
2020-03-10 13:08:50 +0100

    edit global var

--- src/destroyer.js
+++ src/destroyer.js
@@ -1,5 +1,5 @@
 function destroyer(arr) {
-    args = [...arguments].slice(1);
+    const args = [...arguments].slice(1);
     
     return arr.filter( ( val ) => 
     args.includes(val) ? false : true )
